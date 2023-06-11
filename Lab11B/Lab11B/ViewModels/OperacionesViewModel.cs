f7ad9296b2ba8f40980f62020cdcf8e4d720f034
Robert-Espinoza <robert.espinoza.s@tecsup.edu.pe>
2023-06-10 23:11:47 -0500

    evidencias lab11b

---
+++ Lab11B/Lab11B/ViewModels/OperacionesViewModel.cs
@@ -0,0 +1,91 @@
+﻿using Lab11B.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Lab11B.ViewModels
+{
+    public class OperacionesViewModel : ViewModelBase
+    {
+        //public int ValorA { get; set; }
+
+        int valorA;
+        public int ValorA
+        {
+            get { return valorA; }
+            set
+            {
+                if (valorA != value)
+                {
+                    valorA = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+
+        int valorB;
+        public int ValorB
+        {
+            get { return valorB; }
+            set
+            {
+                if (valorB != value)
+                {
+                    valorB = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+        int resultado;
+        public int Resultado
+        {
+            get { return resultado; }
+            set
+            {
+                if (resultado != value)
+                {
+                    resultado = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+
+        public ICommand Sumar { get; set; }
+        public ICommand Restar { get; set; }
+        public ICommand Multiplicar { get; set; }
+        public ICommand Dividir { get; set; }
+
+        public OperacionesViewModel()
+        {
+            Sumar = new Command(() =>
+            {
+                Resultado = ValorA + ValorB;
+            });
+
+
+            Restar = new Command(() =>
+            {
+                Resultado = ValorA - ValorB;
+            });
+
+            Multiplicar = new Command(() =>
+            {
+                Resultado = ValorA * ValorB;
+            });
+
+            Dividir = new Command(() =>
+            {
+                Resultado = ValorA / ValorB;
+            });
+        }
+
+    }
+}
